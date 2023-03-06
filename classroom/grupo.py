3642dcbcb279ed0a06c3ad913c129d44e5b77404
juanma01rs <juanma10rs@gmail.com>
2023-03-06 14:47:21 -0500

    Python 7

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,15 +3,14 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
-        self._grupo = ('Grupo de estudiantes: ' + grupo)
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
+        self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
         for key in kwargs.values():
-            if key not in self._asignaturas:
-                self._asignaturas.append(Asignatura[key])
+            self._asignaturas.append(Asignatura[key])
 
     def agregarAlumno(self, alumno, lista=None):
         if lista == None:
